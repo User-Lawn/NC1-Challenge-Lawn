ebfeeafbcda9d1646727792f0cadcac211354639
이준영 <74142881+User-Lawn@users.noreply.github.com>
2022-05-02 17:01:32 +0900

    feat: SwiftUI file

---
+++ NC1-Lawn-SoMac/NC1-Lawn-SoMac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NC1-Lawn-SoMac
+//
+//  Created by Jun.Mac on 2022/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NC1-Lawn-SoMac/NC1-Lawn-SoMac/NC1_Lawn_SoMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_Lawn_SoMacApp.swift
+//  NC1-Lawn-SoMac
+//
+//  Created by Jun.Mac on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct NC1_Lawn_SoMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
